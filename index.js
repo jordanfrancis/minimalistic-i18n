3e759ac8e397cd0c149ac7b886d9f362cbbd8037
Jordan Francis <13959693+jordanfrancis@users.noreply.github.com>
2019-07-03 22:42:09 +0100

    feat: catch nested invalid path TypeError exception

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 function i18n(path, data) {
-    const val = path
-      .split('.')
-      .reduce(
-        (accumulator, current) => accumulator[current],
-        i18n.locales[i18n.locale]
-      );
-  
+    let val = path.split('.');
+
+    try {
+        val = val.reduce(
+            (accumulator, current) => accumulator[current],
+            i18n.locales[i18n.locale]
+        );
+    } catch (e) {
+        // Catch nested invalid path
+        return path;
+    }
+
     if (!val) {
-      return path;
+        return path;
     }
-  
+
     if (Array.isArray(val)) {
-      return val[data - 1] || val[val.length - 1];
+        return val[data - 1] || val[val.length - 1];
     }
-  
+
     return val
-      .replace(/{[^@}]+}/g, s => data[s.slice(1, -1)])
-      .replace(/{@[^}]+}/g, s => {
-        let n = null;
-        const subpath = s.slice(2, -1).replace(/\(.*\)/, argStr => {
-          n = data[argStr.slice(1, -1)];
-          return '';
+        .replace(/{[^@}]+}/g, s => data[s.slice(1, -1)])
+        .replace(/{@[^}]+}/g, s => {
+            let n = null;
+            const subpath = s.slice(2, -1).replace(/\(.*\)/, argStr => {
+                n = data[argStr.slice(1, -1)];
+                return '';
+            });
+
+            return i18n(subpath, n === null ? data : n);
         });
-  
-        return i18n(subpath, n === null ? data : n);
-      });
-  }
-  
-  i18n.locales = {};
-  i18n.addLocale = (lang, dic) => (i18n.locales[lang] = dic);
-  i18n.setLocale = lang => (i18n.locale = lang);
-  
-  module.exports = { default: i18n, i18n };
-  
+}
+
+i18n.locales = {};
+i18n.addLocale = (lang, dic) => (i18n.locales[lang] = dic);
+i18n.setLocale = lang => (i18n.locale = lang);
+
+module.exports = { default: i18n, i18n };
